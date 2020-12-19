7e3e285044386bc616ebce916ec6b23b3895ef34
HIQBAL202 <64427188+HIQBAL202@users.noreply.github.com>
2020-12-20 01:45:47 +0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ var handleRequest = function(request, response) {
  response.end(" | v=0\n");
  console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - star))
 }
-
 var www = http.createServer(handleRequest);
 www.listen(8080,function () {
  startTime = new Date();;
